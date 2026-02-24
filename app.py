87ad55242b0b0c5d39e9a4544979a8826f21efe0
Martin T Mathew <martintheodorus4@gmail.com>
2026-02-25 00:53:18 +0530

    Register Deacon AI and Mac blueprints
    Import apps.deacon_ai.app and apps.mac.app and register their blueprints on the main Flask app under /deacon_ai and /mac. This modularizes route handling by moving sub-app routes into separate blueprints and wiring them into the primary application.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import flask
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, blueprints
+
+import apps.deacon_ai.app as deacon_ai
+import apps.mac.app as mac
+
 
 app = flask.Flask(__name__)
+
+app.register_blueprint(deacon_ai.bp, url_prefix='/deacon_ai')
+app.register_blueprint(mac.bp, url_prefix='/mac')
 
 @app.route('/', methods=['GET'])
 def hello():
